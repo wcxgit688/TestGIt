1a97c900ca10899e25d0258a2131245ccacf61cf
eclipse_user <eclipse_email>
2021-02-24 19:20:26 +0800

    commit left

--- src/main/java/com/wcx/git/Happy.java
+++ src/main/java/com/wcx/git/Happy.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Happy {
 	public static void main(String[] args) {
-		System.out.println("Git test");
+		System.out.println("Git test left");
 		Scanner sc = new Scanner(System.in);
 	}
 }
