424a5f6469b549f77548edc06a6ad9f003ac6d52
wcx_glo <email_glo@wcx.com>
2021-02-24 19:20:39 +0800

    commit right

--- src/main/java/com/wcx/git/Happy.java
+++ src/main/java/com/wcx/git/Happy.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Happy {
 	public static void main(String[] args) {
-		System.out.println("Git test");
+		System.out.println("Git2 test right");
 		Scanner sc = new Scanner(System.in);
 	}
 }
