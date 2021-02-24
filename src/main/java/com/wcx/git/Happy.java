2186c9bc2dceda41e7b0e0b39c255709eefa1498
eclipse_user <eclipse_email>
2021-02-24 19:32:51 +0800

    edit hot_fix

--- src/main/java/com/wcx/git/Happy.java
+++ src/main/java/com/wcx/git/Happy.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Happy {
 	public static void main(String[] args) {
 		System.out.println("Git test left");
+		System.out.println("hot fix");
 		Scanner sc = new Scanner(System.in);
 	}
 }
